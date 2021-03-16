df0473b945698a1d9eca1373a3cdc88e415377d6
Heather He <1326384411@qq.com>
2021-03-16 13:53:09 +0800

    前后端跨域访问

---
+++ src/main/java/org/middleware/demo1/acitvemq/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.middleware.demo1.acitvemq.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/**
+ * @author 卓
+ * 前后端跨域访问
+ * @date 2021/3/16
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "null")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
