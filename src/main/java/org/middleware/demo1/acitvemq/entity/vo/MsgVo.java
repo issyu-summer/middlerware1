76261213c5fcdd5f4c420408678f0845ce954254
yasuko-an <67579827+yasuko-an@users.noreply.github.com>
2021-03-18 08:49:23 +0800

    修改发送消息格式

---
+++ src/main/java/org/middleware/demo1/acitvemq/entity/vo/MsgVo.java
@@ -0,0 +1,13 @@
+package org.middleware.demo1.acitvemq.entity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class MsgVo {
+    private String msg;
+
+    private Long senderId;
+
+}
