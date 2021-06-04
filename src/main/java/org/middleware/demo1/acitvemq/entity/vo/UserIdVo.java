6415491a31126c9b99fca4442436dadb97a782ef
qqpet24 <710599274@qq.com>
2021-06-04 10:36:11 +0800

    6.4 补充UserIdVo推送

---
+++ src/main/java/org/middleware/demo1/acitvemq/entity/vo/UserIdVo.java
@@ -0,0 +1,15 @@
+package org.middleware.demo1.acitvemq.entity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class UserIdVo {
+    private Long id;
+    private Long groupId;
+}
